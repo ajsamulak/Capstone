4a4e743680b5b477bd702c952ded73b15867eb5e
ajsamulak <100488882+ajsamulak@users.noreply.github.com>
2025-02-08 17:56:12 -0600

    Updated npm

--- src/Componenents/nav.js
+++ src/Componenents/nav.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from 'assets/Logo.svg';
 
 const Nav = () => {
     return (
         <nav>
-
+            <a href="/">
+            <img src={logo} alt="little lemom logo"/>
+            </a>
         </nav>
     )
 }
