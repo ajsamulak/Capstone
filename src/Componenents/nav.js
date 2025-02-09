73be92710ca8aa186185bde5f8205429c8fff8a4
ajsamulak <100488882+ajsamulak@users.noreply.github.com>
2025-02-08 18:20:12 -0600

    nav updated

--- src/Componenents/nav.js
+++ src/Componenents/nav.js
@@ -2,11 +2,45 @@ import React from 'react';
 import logo from 'assets/Logo.svg';
 
 const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen);
+    }
+    
     return (
-        <nav>
-            <a href="/">
+        <nav className={'navbar ${menuOpen ? "open" : ""}'}>
+            <a href="/" className="logo">
             <img src={logo} alt="little lemom logo"/>
             </a>
+            {/* Mobile Navbar */}
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+
+            {/* Nav Items */}
+            <ul className={'nav-links ${menuOpen ? "visible" : ""}'}>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/'>About</a>
+                </li>
+                <li>
+                    <a href='/'>Menu</a>
+                </li>
+                <li>
+                    <a href='/'>Reservations</a>
+                </li>
+                <li>
+                    <a href='/'>Order Online</a>
+                </li>
+                <li>
+                    <a href='/'>Login</a>
+                </li>
+            </ul>
         </nav>
     )
 }
