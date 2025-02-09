586fadae8a5c59de10358cb953531290eaedee1d
ajsamulak <100488882+ajsamulak@users.noreply.github.com>
2025-02-08 18:55:49 -0600

    Updated Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav.js'
+import Header from './components/header.js'
+import Main from './components/main.js'
+import Footer from './components/footer.js'
 
 function App() {
   return (
