d7bac3dff74791849daac4bc1d9d562ee5bd23aa
ajsamulak <100488882+ajsamulak@users.noreply.github.com>
2025-02-10 21:24:19 -0600

    Added recipes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Menu = () => {
     return (
         <div className= 'menu-container'>
-
+            <div className='menu-header'>
+            <h2>This weeks specials</h2>
+            <button>Order Menu</button>
+            </div>
         </div>
     )
 }
