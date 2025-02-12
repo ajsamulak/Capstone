fa034c1ebbc289ae8b8585f6ae6222f2b7031c44
ajsamulak <100488882+ajsamulak@users.noreply.github.com>
2025-02-11 21:53:41 -0600

    updated buttons

--- src/components/CustomersSay.js
+++ src/components/CustomersSay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import stars from "../assets/stars.png"
+import profile from '../assets/profile.png'
 const CustomersSay = () => {
 
         return (
@@ -10,23 +11,27 @@ const CustomersSay = () => {
           <div className="reviews-container">
             <div className="reviews">
                   <img src={stars} alt="Five stars" />
-                      <h5>name</h5>
-                    <p>quote</p>
+                      <h5>Liam</h5>
+                      <img src={profile} alt="Profile avatar" />
+                    <p>"The portions are generous and great for a healthy dinner."</p>
             </div>
             <div className="reviews">
             <img src={stars} alt="Five stars" />
-                      <h5>name</h5>
-                    <p>quote</p>
+                      <h5>Sophia</h5>
+                      <img src={profile} alt="Profile avatar" />
+                    <p>"Loved the vibrant flavors in the bruchetta, a truely refreshing appetizer."</p>
             </div>
             <div className="reviews">
             <img src={stars} alt="Five stars" />
-                      <h5>name</h5>
-                    <p>quote</p>
+                      <h5>Emma</h5>
+                      <img src={profile} alt="Profile avatar" />
+                    <p>"This restaurant is a hidden gem for authentic Mediterranean cuisine."</p>
             </div>
             <div className="reviews">
             <img src={stars} alt="Five stars" />
-                      <h5>name</h5>
-                    <p>quote</p>
+                      <h5>Olivia</h5>
+                      <img src={profile} alt="Profile avatar" />
+                    <p>"The salads are a must-try - juicy veggies, fresh lettuce, and a perfect dressing."</p>
             </div>
             </div>
             </section>
